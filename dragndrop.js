c0f4e120fe0adabff15c74904796aed9bdfb921c
nlovas <nlovas@ualberta.ca>
2016-10-09 23:46:25 -0600

    Replaced all of the draggable buttons with css.

--- dragndrop.js
+++ dragndrop.js
@@ -4,18 +4,28 @@
 //    $( init );
 $(document).ready(function initDragnDrop(){
     //drags and reverts to original position if its not dropped in a valid place
-   /* $('#drag').draggable({cancel:"false",
-        revert: "invalid",
-        cursor: "move",
-        snap : "drop",
-        stack : ".drag"}); // doesnt work?? */
-    $('.makeMeDraggable').draggable({cancel:false, revert: "invalid", cursor: "move", snap: "dropMe", stack: ".makeMeDraggable" });
+    $('.dragndropShape').draggable({cancel:false,
+                                     revert: "invalid",
+                                     cursor: "move",
+                                     stack: ".dragndropShape",
+                                     helper: function(){
+                                             return $("<div class=\"dragndropShape\" id=\"dragndropShape\"> Hello </div>");
+                                         }});
 
     $('.dropMe').droppable( {
-        drop: handleDrop
+        drop: handleDrop,
+        hoverClass: "drop-area"
+       /* over: function(event, ui) {
+            //var property=document.getElementById(btn);
+            $(this).style.backgroundColor=rgb('244,113,33');
+            ui.draggable.data("current-droppable", $(this));
+        }*/
+
     } );
 });
+//receives the drop
 function handleDrop( event, ui ) {
     var draggable = ui.draggable;
     alert( 'The button with ID "' + draggable.attr('id') + '" was dropped onto me!' );
 }
+
