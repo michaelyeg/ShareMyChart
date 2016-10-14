211ed1376ea4e0c8cadffbb3c20cb0861250d72a
nlovas <nlovas@ualberta.ca>
2016-10-14 16:03:05 -0600

    dropping works! Fixed by Diego

--- dragndrop.js
+++ dragndrop.js
@@ -2,30 +2,47 @@
  * Created by User on 2016-10-01.
  */
 //    $( init );
-$(document).ready(function initDragnDrop(){
+
+//var dropHelp = true;
+
+jQuery(document).ready(function initDragnDrop(event, ui){
     //drags and reverts to original position if its not dropped in a valid place
     $('.dragndropShape').draggable({cancel:false,
                                      revert: "invalid",
                                      cursor: "move",
                                      stack: ".dragndropShape",
-                                     helper: function(){
-                                             return $("<div class=\"dragndropShape\" id=\"dragndropShape\"> Hello </div>");
-                                         }});
+                                     hoverClass: "highlightBorder",
+                                     helper: "clone"}); /* function(){
+                                        //helper says the same as the thing it was dragged from
+                                            var dragText = getText(this);
+
+                                             return $("<div class=\"dragndropShape\" id=\"dragndropShape\"></div>");
+                                         }}); */
 
     $('.dropMe').droppable( {
         drop: handleDrop,
         hoverClass: "drop-area"
-       /* over: function(event, ui) {
-            //var property=document.getElementById(btn);
-            $(this).style.backgroundColor=rgb('244,113,33');
-            ui.draggable.data("current-droppable", $(this));
-        }*/
+
 
     } );
 });
+
+
 //receives the drop
 function handleDrop( event, ui ) {
     var draggable = ui.draggable;
+    //$(this).append($(ui.helper.children()));
     alert( 'The button with ID "' + draggable.attr('id') + '" was dropped onto me!' );
+    //event.dataTransfer.setData("Text", event.target.id);
 }
 
+
+//returns the text inside the dragged object
+function getText(obj) {
+return obj.textContent;
+}
+
+//sets the text in an object
+function setText(obj, newtext){
+obj.textContent = newtext;
+}
