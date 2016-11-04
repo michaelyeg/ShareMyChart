249fe4c6632647dc057c27b87b26930dab2329be
bcvancouver <michael-xi@hotmail.com>
2016-11-03 19:20:14 -0600

    aggregate function implemented, hopefully it works.

--- web/graphPicker.js
+++ web/graphPicker.js
@@ -1,6 +1,10 @@
 /**
  * Created by michaelximac on 2016-11-03.
  */
+
+/**
+ * Pick graph based on their data type.
+ */
 function pickGraphType(X,Y) {
     if (X.getType=="nominal" && (Y.getType=="numeric" || Y.getType=="date")){
         aggregate(Y,X);
@@ -12,4 +16,28 @@ function pickGraphType(X,Y) {
     }else if(X.getType=="numeric" && Y.getType=="numeric"){
         displayScatter(X,Y);
     }
+}
+
+/**
+ * Aggregate counts of specified parameters.
+ * @returns {dictionary} Returns a dictionary of aggregateParameter: count.
+ */
+function aggregate(aggPar,par) {
+    // Initialize an associated Array, like dictionary in python.
+    var dictionary=new Array;
+    for (key in dictionary){
+        dictionary.key=value;
+    }
+
+    for (i in aggPar){
+        if (i in dictionary.key){
+            count=dictionary.key;
+            count+=1;
+            dictionary.key=count;
+        }else{
+            dictionary[i.toString()]=1;
+        }
+    }
+
+    return dictionary;
 }
