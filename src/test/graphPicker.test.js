596024c83f570a83347d3e95428f7e245ccfe92e
shadownater <jlovas@ualberta.ca>
2016-11-20 22:32:03 -0700

    Some tests started

---
+++ src/test/graphPicker.test.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Jill - Offline on 2016-11-20.
+ */
+
+
+describe("graphPicker Tests", function() {
+
+    it("Picks bar v chart", function () {
+
+    });
+
+    it("Picks bar h chart", function () {
+
+    });
+
+    it("Picks scatter graph - nom/nom", function () {
+
+    });
+
+    it("Picks scatter graph - num/num", function () {
+
+    });
+
+    it("Picks line chart", function () {
+
+    });
+
+    it("Picks map chart", function () {
+
+    });
+
+});
