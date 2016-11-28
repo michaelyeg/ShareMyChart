f8412a7db047e38a77627b82f4824a1e1565ee4e
Yourui Guo <yourui@ualberta.ca>
2016-11-28 00:08:57 -0700

    ui testing

---
+++ src/test/uploadTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by M.Guo on 27/11/2016.
+ */
+public class uploadTest {
+    public static void main(String[] args) {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://localhost:8080/main.html");
+        WebElement element = driver.findElement(By.id("clicktab1"));
+        element.click();
+        driver.findElement(By.id("myfile")).sendKeys("/Users/Margaret/test.ttl");
+        driver.findElement(By.id("form-submit")).submit();
+    }
+}
