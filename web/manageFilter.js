fbe76c5eadce5df4f2a4b3216ad7303e1cb77855
bcvancouver <michael-xi@hotmail.com>
2016-11-27 19:15:46 -0700

    fix some issues

--- web/manageFilter.js
+++ web/manageFilter.js
@@ -16,39 +16,6 @@ function initFilter (){
     document.getElementById("dropup1").style.display = "block";
     document.getElementById("delete1").style.display = "block";
     document.getElementById("updatebutton").style.display = "block";
-    var filterArray=new filterArray();
-}
-
-$("#filterX").click(function(){
-
-    var _html = '<div class="input-group filter" id='+ count +'> <span class="input-group-addon"> <input type="checkbox" class="check"> </span> ' +
-        '<span class="input-group-addon" id="sizing-addon3">X - axis</span> ';
-    _html += '<select name="priceCondition"> <option value="<"><</option> <option value="=">=</option>' +
-        ' <option value=">">></option> </select>';
-
-    var date = '<input type="date" value="0" size="4"><br>';
-    var num = '<input type="number" value="0" size="4"><br>';
-
-    if (xT == "date"){
-        $("#filterForm").append(_html+date+"</div>");
-    }
-    else $("#filterForm").append(_html+num+"</div>");
-    count++;
-    //TODO: Connect backend
-var xT,yT;
-var count = 0;
-
-function getParaType(xType, yType){
-    xT = xType;
-    yT = yType;
-}
-
-
-function initFilter (){
-    console.log("~~~~~~~~~~~block");
-    document.getElementById("dropup1").style.display = "block";
-    document.getElementById("delete1").style.display = "block";
-    document.getElementById("updatebutton").style.display = "block";
 }
 
 $("#filterX").click(function(){
